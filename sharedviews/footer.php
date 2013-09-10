0a92f308befd4405f2e3dd08b4c3917f0dccdbe0
adamaoc <adamaoc@gmail.com>
2013-09-10 06:04:09 -0500

    updated header and footer files

--- sharedviews/footer.php
+++ sharedviews/footer.php
@@ -6,10 +6,6 @@
       </div>
     </div>
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
     <script src="/js/jquery.js"></script>
     <script src="/js/bootstrap.min.js"></script>
 
--- sharedviews/header.php
+++ sharedviews/header.php
@@ -6,11 +6,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
+    <meta name="description" content="Online shopping for all your seasonal or holiday decorations. Great for the home or office and sold at amazing prices!">
+    <meta name="author" content="ampnetmedia.com">
+    <link rel="shortcut icon" href="/img/favicon.png">
 
-    <title>Holiday Design - Shopping for the holidays</title>
+    <title>Holiday Design - Online Shopping for the holidays.</title>
+    <?php // make a getTitle() function that dynamically displays title name for diff pages. ?>
     <link href="/css/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
     <!--[if IE]>
         <link href="/css/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
